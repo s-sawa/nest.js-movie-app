8b798ddf9cb9881f2eba552412d2662784909130
s-sawa <s.s.hrmeee21@gmail.com>
2023-12-27 09:02:53 +0900

    ORM,database準備

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
