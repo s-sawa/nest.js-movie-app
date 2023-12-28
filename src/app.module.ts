e45125d027eb95e3d692a3a23aa37a7b9953e70a
s-sawa <s.s.hrmeee21@gmail.com>
2023-12-28 11:37:20 +0900

    implement user creation functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MoviesModule, TypeOrmModule.forRoot()],
+  imports: [MoviesModule, TypeOrmModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })
--- src/movies/movie.repository.ts
+++ src/movies/movie.repository.ts
@@ -4,18 +4,18 @@ import { CreateMovieDto } from './DTO/create-movie.dto';
 
 @EntityRepository(Movie)
 export class MovieRepository extends Repository<Movie> {
-    async createMovie(createMovieDto: CreateMovieDto): Promise<Movie> {
-        const {title, description, rating, review} = createMovieDto;
-        const movie = this.create({
-            title,
-            description,
-            rating,
-            review,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-        });
+  async createMovie(createMovieDto: CreateMovieDto): Promise<Movie> {
+    const { title, description, rating, review } = createMovieDto;
+    const movie = this.create({
+      title,
+      description,
+      rating,
+      review,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
 
-        await this.save(movie);
-        return movie;
-    }
+    await this.save(movie);
+    return movie;
+  }
 }
