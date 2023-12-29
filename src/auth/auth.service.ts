f16dd09ff23916c62a8a8d627b55c14d7924f54d
s-sawa <s.s.hrmeee21@gmail.com>
2023-12-29 09:29:35 +0900

    jwtトークン実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'src/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { CredentialsDto } from './dto/credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
     return await this.userRepository.createUser(createUserDto);
+  }
+
+  async signIn(
+    credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = credentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    // 認証に問題ないかチェック
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { id: user.id, username: user.username };
+      const accessToken = await this.jwtService.sign(payload); // 署名済みtokenが作成される
+      return { accessToken };
+    }
+    throw new UnauthorizedException('ユーザー名またはパスワードが異なります');
   }
 }
